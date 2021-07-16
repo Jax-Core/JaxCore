7e44facc246549769eade8de6f4be6d28a17e9c3
Jax <80020581+EnhancedJax@users.noreply.github.com>
2021-07-16 16:44:00 +0800

    Merged all skins.

--- @Resources/Lua/GenCtx.lua
+++ @Resources/Lua/GenCtx.lua
@@ -5,6 +5,7 @@ function Update()
 	local Center = tonumber(SKIN:GetVariable('Sec.Ctx.Center', '0'))
 	local Blur = tonumber(SKIN:GetVariable('Sec.Ctx.Blur', '0'))
 	local Settings = tonumber(SKIN:GetVariable('Sec.Ctx.Settings', '1'))
+	local Unload = tonumber(SKIN:GetVariable('Sec.Ctx.Unload', '0'))
 	local SAW = tonumber(SKIN:GetVariable('SCREENAREAWIDTH'))
 	local CCW = tonumber(SKIN:GetVariable('CURRENTCONFIGWIDTH'))
 	local SAH = tonumber(SKIN:GetVariable('SCREENAREAHEIGHT'))
@@ -48,7 +49,7 @@ function Update()
 		,'MeterStyle=Ctx.String:S | CtxText:S\n')
 		end
 
-	if (Blur == 1 or Settings == 1) then
+	if (Blur == 1 or Settings == 1 or Unload == 1) then
 		File:write('[Divider1]\n'
 		,'Meter=Shape\n'
 		,'MeterStyle=Ctx.Div:S\n')
@@ -89,7 +90,24 @@ function Update()
 		,'Text=Settings\n'
 		,'MeterStyle=Ctx.String:S | CtxText:S\n')
 		end
-
+	
+	if Unload == 1 then
+		File:write('[Unload]\n'
+		,'Meter=Shape\n'
+		,'MeterStyle=CtxBox:S\n'
+		,'Fill=Fill Color #Set.Pri_Color#,0\n'
+		,'MouseOverAction=[!SetOption #CURRENTSECTION# Fill "Fill Color #Set.Pri_Color#,110"][!SetOption #CURRENTSECTION#Icon ImageTint "234,234,230"][!SetOption 22 FontColor "234,234,230"][!UpdateMeter *][!Redraw]\n'
+		,'MouseLeaveAction=[!SetOption #CURRENTSECTION# Fill "Fill Color #Set.Pri_Color#,0"][!SetOption #CURRENTSECTION#Icon ImageTint "#Set.Pri_Color#"][!SetOption 22 FontColor "#Set.Pri_Color#"][!UpdateMeter *][!Redraw]\n'
+		,'LeftMouseUpAction=[!UpdateMeasure Unload "#Sec.Skin#\\Main"][!DeactivateConfig]\n'
+		,'DynamicVariables=1\n'
+		,'[UnloadIcon]\n'
+		,'Meter=Image\n'
+		,'MeterStyle=CtxIcon:S\n'
+		,'[22]\n'
+		,'Meter=String\n'
+		,'Text=Unload\n'
+		,'MeterStyle=Ctx.String:S | CtxText:S\n')
+		end
 	File:close()
 
 	end
