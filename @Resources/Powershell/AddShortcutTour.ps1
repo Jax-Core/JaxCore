1270b38e15f93e07d3193f934a969b7ce3d1e1c1
Jax <80020581+EnhancedJax@users.noreply.github.com>
2021-12-02 18:12:47 +0800

    New tour

---
+++ @Resources/Powershell/AddShortcutTour.ps1
@@ -0,0 +1,52 @@
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$Startpath = $env:APPDATA
+
+function Check {
+    If (Test-Path -Path "$Startpath\Microsoft\Windows\Start Menu\Programs\JaxCore.lnk") {
+        $RmAPI.Bang('[!SetOption Card1.Button2.Shape MeterStyle "CardSquareButton:S | Card1.ButtonProg.True"]')
+        $RmAPI.Bang('[!UpdateMeter Card1.Button2.Shape]')
+        $RmAPI.Bang('[!Redraw]')
+        $RmAPI.Log("Found: CoreHome in programs")
+    } else {
+        $RmAPI.Log("Failed to find corehome in programs")
+    }
+    If (Test-Path -Path "$DesktopPath\JaxCore.lnk") {
+        $RmAPI.Bang('[!SetOption Card1.Button1.Shape MeterStyle "CardSquareButton:S | Card1.ButtonDesk.True"]')
+        $RmAPI.Bang('[!UpdateMeter Card1.Button1.Shape]')
+        $RmAPI.Bang('[!Redraw]')
+        $RmAPI.Log("Found: CoreHome on desktop")
+    } else {
+        $RmAPI.Log("Failed to find corehome on desktop")
+    }
+
+}
+
+function Desktop {
+    $RainmeterExe = $RmAPI.VariableStr('PROGRAMPATH')
+    $ResourceFolder = $RmAPI.VariableStr('@')
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut("$DesktopPath\JaxCore.lnk")
+    $Shortcut.TargetPath = $RainmeterExe+"Rainmeter.exe"
+    $Shortcut.Arguments = '!ActivateConfig #JaxCore\Main Home.ini'
+    $shortcut.IconLocation = $ResourceFolder+"Images\Logo.ico"
+    $Shortcut.Save()
+}
+
+function StartFolder {
+    $RainmeterExe = $RmAPI.VariableStr('PROGRAMPATH')
+    $ResourceFolder = $RmAPI.VariableStr('@')
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut("$Startpath\Microsoft\Windows\Start Menu\Programs\JaxCore.lnk")
+    $Shortcut.TargetPath = $RainmeterExe+"Rainmeter.exe"
+    $Shortcut.Arguments = '!ActivateConfig #JaxCore\Main Home.ini'
+    $shortcut.IconLocation = $ResourceFolder+"Images\Logo.ico"
+    $Shortcut.Save()
+}
+
+function RemoveDeskop {
+    Remove-Item "$DesktopPath\JaxCore.lnk" -Recurse
+}
+
+function RemoveStartFolder {
+    Remove-Item "$Startpath\Microsoft\Windows\Start Menu\Programs\JaxCore.lnk" -Recurse
+}
--- Accessories/UpdatePrompt/Logic.lua
+++ Accessories/UpdatePrompt/Logic.lua
@@ -10,8 +10,8 @@ function check()
     else
         print('Update required - '..ParsedVer..'>='..CoreVer)
         SKIN:Bang('!CommandMeasure', 'CheckForDataFolder' ,'Check-Data')
-        SKIN:Bang('!WriteKeyValue', 'Variables', 'ParsedVer', ParsedVerFull, SKIN:GetVariable('ROOTCONFIGPATH')..'Accessories\\Notification\\Toast\\Main.ini')
-        SKIN:Bang('!ActivateConfig', '#JaxCore\\Accessories\\Notification\\Toast')
+        SKIN:Bang('!WriteKeyValue', 'Variables', 'ParsedVer', ParsedVerFull, SKIN:GetVariable('ROOTCONFIGPATH')..'Accessories\\UpdatePrompt\\Toast\\Main.ini')
+        SKIN:Bang('!ActivateConfig', '#JaxCore\\Accessories\\UpdatePrompt\\Toast')
     end
 end
 
@@ -31,8 +31,8 @@ end
 
 function runUpdate()
     ParsedVerFull = SKIN:GetVariable('ParsedVer')
-    SKIN:Bang('!WriteKeyValue', 'Variables', 'ParsedVer', '0', SKIN:GetVariable('ROOTCONFIGPATH')..'Accessories\\Notification\\Toast\\Main.ini')
-    SKIN:Bang('!SetVariable', 'DownloadLink', 'https://github.com/EnhancedJax/JaxCore/releases/download/v'..ParsedVerFull..'/JaxCore_v'..ParsedVerFull..'.rmskin\n')
+    SKIN:Bang('!WriteKeyValue', 'Variables', 'ParsedVer', '0', SKIN:GetVariable('ROOTCONFIGPATH')..'Accessories\\UpdatePrompt\\Toast\\Main.ini')
+    SKIN:Bang('!SetVariable', 'DownloadLink', 'https://github.com/Jax-Core/JaxCore/releases/download/v'..ParsedVerFull..'/JaxCore_v'..ParsedVerFull..'.rmskin\n')
     SKIN:Bang('!SetVariable', 'DownloadName', 'JaxCore'..ParsedVerFull)
     SKIN:Bang('!CommandMeasure', 'Installer', 'Install')
 end
