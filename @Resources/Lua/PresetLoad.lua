4e38fb869c5f2fc40ac4881bac03a55a76d21ec8
Jax <80020581+EnhancedJax@users.noreply.github.com>
2022-01-12 22:26:21 +0800

    Update PresetLoad.lua

--- @Resources/Lua/PresetLoad.lua
+++ @Resources/Lua/PresetLoad.lua
@@ -1,8 +1,11 @@
+sectionTable = {}
+
 function ReadIni(inputfile)
     local file = assert(io.open(inputfile, 'r'), 'Unable to open ' .. inputfile)
     local tbl, section = {}
     local sectionReadOrder, keyReadOrder = {}, {}
     local num = 0
+    local sectionNum = 0
     for line in file:lines() do
         --print(line)
         num = num + 1
@@ -11,6 +14,8 @@ function ReadIni(inputfile)
             if line:match('^%s-%[.+') then
                 section = line:match('^%s-%[([^%]]+)')
                 if not tbl[section] then
+                    sectionNum = sectionNum + 1
+                    table.insert(sectionTable, section)
                     tbl[section]={}
                     table.insert(sectionReadOrder, section)
                     if not keyReadOrder[section] then keyReadOrder[section]={} end
@@ -44,13 +49,49 @@ function tablelength(T)
     return count
   end
 
-function ChangeTo(theme)
-    Skinpath = SKIN:GetVariable('SKINSPATH')
-    SkinName = SKIN:GetVariable('Skin.Name')
-    if SkinName == nil then SkinName = '#JaxCore' end
-    ini = ReadIni(Skinpath..SkinName..'\\@Resources\\Presets\\'..theme..'.inc')
-    saveLocation = Skinpath..SkinName..'\\@Resources\\Vars.inc'
-    for i = 1, tablelength(ini.INI['Values']['keys']) do
-        SKIN:Bang('!WriteKeyValue', 'Variables', ini.INI['Values']['keys'][i], ini.INI['Values']['values'][i], saveLocation)
+  function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
     end
-end
+ end
+
+  function ChangeTo(theme)
+      Skinpath = SKIN:GetVariable('SKINSPATH')
+      SkinName = SKIN:GetVariable('Skin.Name')
+      if SkinName == nil then SkinName = '#JaxCore' end
+      ini = ReadIni(Skinpath..SkinName..'\\@Resources\\Presets\\'..theme..'.inc')
+      saveLocation = Skinpath..SkinName..'\\@Resources\\Vars.inc'
+      for i = 1, tablelength(ini.INI['Values']['keys']) do
+          SKIN:Bang('!WriteKeyValue', 'Variables', ini.INI['Values']['keys'][i], ini.INI['Values']['values'][i], saveLocation)
+      end
+  end
+
+  function ChangeToFiles(theme)
+      Skinpath = SKIN:GetVariable('SKINSPATH')
+      SkinName = SKIN:GetVariable('Skin.Name')
+      if SkinName == nil then SkinName = '#JaxCore' end
+      Defaultini = ReadIni(Skinpath..SkinName..'\\@Resources\\Presets\\DEFAULT.inc')
+      ini = ReadIni(Skinpath..SkinName..'\\@Resources\\Presets\\'..theme..'.inc')
+      for i = 1, tablelength(Defaultini.INI) do
+        local saveLocation = sectionTable[i]
+        for i = 1, tablelength(Defaultini.INI[saveLocation]['keys']) do
+            SKIN:Bang('!WriteKeyValue', 'Variables', Defaultini.INI[saveLocation]['keys'][i], Defaultini.INI[saveLocation]['values'][i], Skinpath..SkinName..'\\'..saveLocation)
+        end
+      end
+
+      for i = tablelength(Defaultini.INI) + 1, tablelength(ini.INI) + tablelength(Defaultini.INI)do
+        local saveLocation2 = sectionTable[i]
+        for i = 1, tablelength(ini.INI[saveLocation2]['keys']) do
+            SKIN:Bang('!WriteKeyValue', 'Variables', ini.INI[saveLocation2]['keys'][i], ini.INI[saveLocation2]['values'][i], Skinpath..SkinName..'\\'..saveLocation2)
+        end
+      end
+  end
+
+  
