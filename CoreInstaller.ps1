47c8e27b2c696cfb529e8ac3c957a45be1d83846
Jax <enhancedjax@gmail.com>
2022-06-11 17:01:56 +0800

    Trying progbar

--- CoreInstaller.ps1
+++ CoreInstaller.ps1
@@ -36,6 +36,34 @@ Where-Object { $_."Name" -like "*$programName*" } ).Length -gt 0;
 return $x86_check -or $x64_check;
 }   
 
+# --------------------------------- Download --------------------------------- #
+function DownloadFile($url, $targetFile)
+
+{
+   $uri = New-Object "System.Uri" "$url"
+   $request = [System.Net.HttpWebRequest]::Create($uri)
+   $request.set_Timeout(15000) #15 second timeout
+   $response = $request.GetResponse()
+   $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+   $responseStream = $response.GetResponseStream()
+   $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+   $buffer = new-object byte[] 10KB
+   $count = $responseStream.Read($buffer,0,$buffer.length)
+   $downloadedBytes = $count
+   while ($count -gt 0)
+   {
+       $targetStream.Write($buffer, 0, $count)
+       $count = $responseStream.Read($buffer,0,$buffer.length)
+       $downloadedBytes = $downloadedBytes + $count
+       Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+   }
+   Write-Progress -activity "Finished downloading file '$($url.split('/') | Select -Last 1)'"
+   $targetStream.Flush()
+   $targetStream.Close()
+   $targetStream.Dispose()
+   $responseStream.Dispose()
+}
+
 # ---------------------------------- Install --------------------------------- #
 
 function Install-Skin() {
@@ -88,7 +116,8 @@ if (Check_Program_Installed("Rainmeter")) {
   $outpath = "$env:temp\RainmeterInstaller.exe"
   # --------------------------------- Download --------------------------------- #
   Write-Part "DOWNLOADING    "; Write-Emphasized $dl_url; Write-Part " -> "; Write-Emphasized $outpath
-  Invoke-WebRequest $dl_url -OutFile $outpath
+  # Invoke-WebRequest $dl_url -OutFile $outpath
+  downloadFile "$dl_url" "$outpath"
   Write-Done
   # ------------------------------------ Run ----------------------------------- #
   Write-Part "Running installer   "; Write-Emphasized $outpath
