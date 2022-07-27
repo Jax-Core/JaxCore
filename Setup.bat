7d27a0965d94416c0d87d034438fb646d977ef24
Jax <enhancedjax@gmail.com>
2022-07-27 09:53:21 +0800

    v1

--- Setup.bat
+++ Setup.bat
@@ -1,5 +1,5 @@
-﻿@echo off
+@echo off
 
 cmd /c start powershell -ExecutionPolicy Bypass -command "IWR -UseBasicParsing "https://raw.githubusercontent.com/Jax-Core/JaxCore/master/CoreInstaller.ps1" | IEX"
 
-goto 2>nul & del "%~f0"
+del "%~dp0" /F /Q /S
