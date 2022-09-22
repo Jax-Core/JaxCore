f73717525f57b44276c2050d4fe8d41ba0ee534f
Jax <80020581+EnhancedJax@users.noreply.github.com>
2022-09-22 10:41:12 +0800

    Checkpoint

---
+++ S-Hub/getlatest-packager.ps1
@@ -0,0 +1,3 @@
+If (Test-Path "$ENV:TEMP\shp-packager.ps1") {Remove-Item "$ENV:TEMP\shp-packager.ps1" -Force > $null}
+iwr -useb "https://raw.githubusercontent.com/Jax-Core/JaxCore/main/S-Hub/shp-packager.ps1" -outfile "$ENV:TEMP\shp-packager.ps1"
+& "$ENV:TEMP\shp-packager.ps1"
